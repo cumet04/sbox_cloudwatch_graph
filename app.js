6ed620bca6ff371c04b32ab5c813ec00505851ce
medalhkr <medalhkr@gmail.com>
2018-11-22 16:31:54 +0900

    run

--- app.js
+++ app.js
@@ -1,42 +1,36 @@
 const AWS = require("aws-sdk");
 const fs = require("fs");
 
-const test = async () => {
-  const props = {
-    width: 500,
-    height: 400,
-    start: "-PT3H",
-    end: "PT0H",
-    view: "timeSeries",
-    stacked: false,
-    metrics: [
-      ["AWS/EC2", "CPUUtilization", "AutoScalingGroupName", "AS_GROUP_NAME"],
-      [
-        "AWS/EC2",
-        "NetworkOut",
-        "InstanceId",
-        "i-XXXX",
-        {
-          yAxis: "right"
-        }
-      ]
-    ],
-    stat: "Average",
-    period: 60
-  };
-  const widgetDefinition = {
-    MetricWidget: JSON.stringify(props)
-  };
+async function getMetricImage(props, output) {
+    try {
+        const result = await new AWS.CloudWatch()
+            .getMetricWidgetImage({ MetricWidget: JSON.stringify(props) })
+            .promise();
+        console.log(result);
+        fs.writeFileSync(output, result.MetricWidgetImage);
+    } catch (e) {
+        console.log(e);
+    }
+}
 
-  const cloudwatch = new AWS.CloudWatch();
-  try {
-    const result = await cloudwatch
-      .getMetricWidgetImage(widgetDefinition)
-      .promise();
-    console.log(result);
-    fs.writeFileSync("./test1.png", result.MetricWidgetImage);
-  } catch (e) {
-    console.log(e);
-  }
-};
-test();
+getMetricImage(
+    {
+        width: 500,
+        height: 400,
+        start: "-PT3H",
+        end: "PT0H",
+        view: "timeSeries",
+        stacked: false,
+        metrics: [
+            [
+                "AWS/EC2",
+                "CPUUtilization",
+                "AutoScalingGroupName",
+                "AS_GROUP_NAME"
+            ]
+        ],
+        stat: "Average",
+        period: 60
+    },
+    "./test.png"
+);
